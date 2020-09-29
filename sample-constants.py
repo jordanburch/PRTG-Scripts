2f9b2f3d346f2209ae814107f55ac73e2a208b11
Patrick Barron <patrick@yobo.dev>
2020-09-29 11:15:04 -0400

    Add sample constants.py

---
+++ sample-constants.py
@@ -0,0 +1,11 @@
+# This is an example of what a constants.py file would look like:
+
+def get_constants():
+    return {
+        "PRTG_URL": "http://your.prtg.domain/api/",
+        "PRTG_USER": "API_User",
+        "PRTG_PASSHASH": "123456789",
+        "PRTG_CLONE_DEVICE": 0,
+        "PRTG_CLONE_GROUP": 0,
+        "PRTG_AUTOMATION_GROUP": 0
+    }
